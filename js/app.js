947e708ebc84e3a00ca6d5df1d2f53a6aa52cf59
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2016-04-22 22:38:13 -0400

    Updates JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-$(document).ready(function() 
-	$("#AddButton").click(addItem);
-	$("#itemTextBox").keydown(function(enter) {
-		if(enter.keycode == 13) {
-			addItem();
-		}
+$(document).ready(function() {
+	$('#itemForm').submit(function(e){
+		e.preventDefault();
+		addItem();
 	});
 
-	
+	$('.list-scope').on('click','.delete',function(){
+		$(this).parent().remove();
+	});
+
+	$('.list-scope').on('click','.check',function(){
+		$(this).parent().toggleClass('checked');
+	});
+});
+
+function addItem(){
+	var item = $('#itemTextBox').val();
+	$('#itemTextBox').val('');
+
+	var el = '<p>'+ item +'<span class="check">&#x2714</span><span class="delete">&#x2718</span></p>';
+
+	$('.list-scope').prepend(el);
+}
